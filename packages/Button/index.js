decd7f1bc5adb5581d8b444e560570ac35c30cfb
Mikey Murphy <mikeyamadeo@gmail.com>
2017-07-25 17:36:13 -0600

    em > px

--- packages/Button/index.js
+++ packages/Button/index.js
@@ -14,28 +14,17 @@ const baseStyle = css({
 })
 
 const sizing = {
-  big: css({
-    height: pxToEm(48),
-    width: pxToEm(194),
-    fontSize: '14px'
-  }),
-  small: css({
-    height: pxToEm(40),
-    width: pxToEm(138),
-    fontSize: '13px'
-  })
+  big: css({ height: pxToEm(48), width: pxToEm(194), fontSize: pxToEm(14) }),
+  small: css({ height: pxToEm(40), width: pxToEm(138), fontSize: pxToEm(13) })
 }
 
-const createTheme = ({primary, light, dark}) => css({
-  color: colors.white,
-  backgroundColor: primary,
-  ':hover': {
-    backgroundColor: light
-  },
-  ':active': {
-    backgroundColor: dark
-  }
-})
+const createTheme = ({ primary, light, dark }) =>
+  css({
+    color: colors.white,
+    backgroundColor: primary,
+    ':hover': { backgroundColor: light },
+    ':active': { backgroundColor: dark }
+  })
 
 const theme = {
   primary: createTheme({
@@ -43,54 +32,46 @@ const theme = {
     light: colors.lightPrimary,
     dark: colors.darkPrimary
   }),
-
   secondary: createTheme({
     primary: colors.tertiary,
     light: colors.lightTertiary,
     dark: colors.darkTertiary
   }),
-
   alert: createTheme({
     primary: colors.alert,
     light: colors.lightAlert,
     dark: colors.darkAlert
   }),
-
   text: css({
     color: colors.darkGrey,
     backgroundColor: 'transparent',
-    ':hover': {
-      color: colors.grey
-    },
-    ':active': {
-      color: colors.darkestGrey
-    }
+    ':hover': { color: colors.grey },
+    ':active': { color: colors.darkestGrey }
   })
 }
 
 const whiteListedProps = {
-  id: value => ({id: value}),
-  baseRef: value => ({ref: value}),
-  onClick: onClick => ({onClick}),
-  type: type => ({type})
+  id: value => ({ id: value }),
+  baseRef: value => ({ ref: value }),
+  onClick: onClick => ({ onClick }),
+  type: type => ({ type })
 }
 
-const Button = ({
-  theme: th = 'primary',
-  small,
-  children,
-  ...rest
-}) =>
+const Button = ({ theme: th = 'primary', small, children, ...rest }) => (
   <button
     {...transformProps(whiteListedProps, rest)}
-    {...normalize} {...baseStyle} {...theme[th]} {...sizing[small ? 'small' : 'big']}
+    {...normalize}
+    {...baseStyle}
+    {...theme[th]}
+    {...sizing[small ? 'small' : 'big']}
   >
     {children}
   </button>
+)
 
 Button.propTypes = {
   children: pt.string.isRequired,
-  theme: pt.oneOf(['primary', 'secondary', 'alert', 'text']),
+  theme: pt.oneOf([ 'primary', 'secondary', 'alert', 'text' ]),
   small: pt.bool
 }
 
