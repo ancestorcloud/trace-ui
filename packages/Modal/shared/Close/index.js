752e82f9e62eaad0a366747d2a0c3277ca2b07e9
Mikey Murphy <mikeyamadeo@gmail.com>
2017-08-07 15:13:07 -0600

    formatting + update to latest glamor api

--- packages/Modal/shared/Close/index.js
+++ packages/Modal/shared/Close/index.js
@@ -6,19 +6,15 @@ import { pxToEm } from '../../../style/utils'
 const close = css({
   cursor: 'pointer',
   transition: '.25s',
-  ':hover': {
-    transform: 'scale(1.1)'
-  }
+  ':hover': { transform: 'scale(1.1)' }
 })
 
-const size = (sz) => css({
-  width: pxToEm(sz),
-  height: pxToEm(sz)
-})
+const size = sz => css({ width: pxToEm(sz), height: pxToEm(sz) })
 
-const Close = ({onClick, size: sz = 20}) =>
-  <div {...{onClick}} {...size(sz)} {...close}>
-    <Cl size='100%'/>
+const Close = ({ onClick, size: sz = 20 }) => (
+  <div {...{ onClick }} {...size(sz)} {...close}>
+    <Cl size='100%' />
   </div>
+)
 
 export default Close
